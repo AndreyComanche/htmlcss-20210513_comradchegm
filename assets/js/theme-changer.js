18698f900a955dd7a3db3d134d8ec2c4b324c68c
AndreyComanche <comradchegm@gmail.com>
2021-06-24 21:31:48 +0500

    fix storage dark theme

--- assets/js/theme-changer.js
+++ assets/js/theme-changer.js
@@ -2,9 +2,22 @@ const changerHdr = document.getElementById('theme-changer-hdr');
 const changerBrg = document.getElementById('theme-changer-brg');
 const html = document.documentElement;
 
+let currentTheme = localStorage.getItem('theme') || 'light';
+if (currentTheme === 'dark') {
+    html.setAttribute('data-theme-dark', 'true');
+    changerHdr.checked = true;
+    changerBrg.checked = true;
+}
+
 toggleDark = function (event) {
     target = event.currentTarget;
-    html.toggleAttribute('data-theme-dark');
+    if (target.checked) {
+        html.setAttribute('data-theme-dark', 'true');
+        localStorage.setItem('theme', 'dark');
+    } else {
+        html.removeAttribute('data-theme-dark');
+        localStorage.setItem('theme', 'light');
+    }
     if (target === changerHdr) {
         if (target.checked !== changerBrg.checked) {
             changerBrg.checked = target.checked;
